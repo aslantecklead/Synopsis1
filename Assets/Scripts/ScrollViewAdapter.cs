0d2615e1f99b54fbb08beda1806d611c0b095bd4
aslantecklead <97795768+aslantecklead@users.noreply.github.com>
2023-02-01 17:00:24 +0300

    Init

---
+++ Assets/Scripts/ScrollViewAdapter.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5097425453ee977b243267c8f492e38a6391a2973df9e964dc533c25a55478a7
+size 4665
---
+++ Assets/Scripts/ScrollViewAdapterConnected.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4be41822ebb040375a7cc240f2b3018c3be053bb4c633148d37e2132da51600
+size 5282
